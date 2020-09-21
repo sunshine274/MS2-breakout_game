16b9b526916c41c223ec5ad8a6e729f56f37a802
sunshine274 <jingx274@gmail.com>
2020-09-21 09:05:23 +0000

    Drawing everything in JS

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const paddle = {
 const brickInfo = {
     w: 70,
     h: 20,
-    padding:10,
+    padding: 10,
     offsetX: 45,
     offsetY: 60,
     visible: true
@@ -41,8 +41,9 @@ const bricks = [];
 for(let i = 0; i < brickRowCount; i ++) {
     bricks[i] = [];
     for(let j = 0; j< brickColumnCount; j++) {
-const x = i * (brickInfo.w + brickInfo.padding)
-const y = j * (brickInfo.h + brickInfo.)
+const x = i * (brickInfo.w + brickInfo.padding)+ brickInfo.offsetX;
+const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+bricks[i][j] = {x,y,...brickInfo}
     }
 }
 
@@ -64,17 +65,33 @@ ctx.fill();
 ctx.closePath();
 }
 
-// Draw everything
-function draw() {
-    drawBall();
-    drawPaddle();
-    drawScore();
-}
+
 
 // Draw score on canvas
 function drawScore() {
     ctx.font = '20px Arial';
     ctx.fillText(`Score: ${score}`,canvas.width - 100, 30);
+}
+
+// Draw bricks on canvas
+function drawBricks() {
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            ctx.beginPath();
+            ctx.rect(brick.x, brick.y, brick.w, brick.h);
+            ctx.fillStyle = brick.visible ? '#0095dd' : 'transparent';
+            ctx.fill();
+            ctx.closePath();
+        });
+    });
+}
+
+// Draw everything
+function draw() {
+    drawBall();
+    drawPaddle();
+    drawScore();
+    drawBricks();
 }
 
 draw();
